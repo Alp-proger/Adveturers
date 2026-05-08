e0435018609b246e59bbe14d454a75ddbf4999e6
olshanskiy <olshanskiy@DEV-25>
2026-05-08 11:09:05 +0300

    ssample

---
+++ AdventurersUI/AvaloniaApplication1/ViewModels/SimpleViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public class SimpleViewModel : INotifyPropertyChanged
+    {
+        private string? _Name; 
+        public string? Name 
+        { 
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (Name != value)
+                {
+                    _Name = value;
+                }
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(Greeting));
+            }
+        }
+        public string Greeting
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Name))
+                {
+                    return "Hello World from Avalonia.Samples";
+                }
+                else
+                {
+                    return $"Hello {Name}";
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }    
+}
